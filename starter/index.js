a3bd1e1ebba8807ffa047a8a32d99e4edce79723
Donjella <brendonang@gmail.com>
2025-06-26 19:18:27 +1000

    feat(server): add dynamic product page rendering based on query id

--- starter/index.js
+++ starter/index.js
@@ -53,12 +53,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj= JSON.parse(data);
   
-
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+     const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
-    if(pathName === '/' || pathName === '/overview'){
+    if(pathname === '/' || pathname === '/overview'){
         res.writeHead(200, {'content-type': 'text/html'});
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
@@ -66,12 +65,15 @@ const server = http.createServer((req, res) => {
         res.end(output);
 
     // Product page
-    } else if (pathName === '/product') {
+    } else if (pathname === '/product') {
+    
         res.writeHead(200, {'content-type': 'text/html'});
-        res.end(tempProduct);
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output)
 
     // API 
-    } else if (pathName === '/api') {
+    } else if (pathname === '/api') {
         res.writeHead(200, {'content-type': 'application/json'});
         res.end(data);
 
